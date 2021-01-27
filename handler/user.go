7aa37a3bd88fc862f238f96a40701ab358d8ae29
hdextor <fahadhafiz5@gmail.com>
2021-01-27 16:53:14 +0700

    Login endpoint

--- handler/user.go
+++ handler/user.go
@@ -52,3 +52,44 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 // tangkap inputan dari user
 // map input dari user ke struct RegisterUserInput
 // struct di atas kita passing sebagai parameter service
+
+func (h *userHandler) Login(c *gin.Context) {
+	//user memasukan input (username dan password)
+	//input ditangkap handler
+	//mapping dari input user ke input struct
+	//input struct passing service
+	//diservice mencari dg bantuan repository user dengan email
+	//mencocokan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+
+		errors := helper.FormatValidationError(err)
+
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentokentoken")
+
+	response := helper.APIResponse("Login success", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
--- user/service.go
+++ user/service.go
@@ -1,11 +1,14 @@
 package user
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
+	Login(input LoginInput) (User, error)
 }
 
 type service struct {
@@ -41,3 +44,23 @@ func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
 
 // mapping struct input ke struct user
 // simpan struct melalui repository
+
+func (s *service) Login(input LoginInput) (User, error) {
+	email := input.Email
+	password := input.Password
+
+	user, err := s.repository.FindByEmail(email)
+	if err != nil {
+		return user, err
+	}
+	if user.ID == 0 {
+		return user, errors.New("User not found on this email")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
